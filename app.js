7c07d9d4a5754601f7f23956f82b69000eb27280
dkssud <dkssud@yandex.ru>
2022-11-15 22:35:56 +0300

    task 15-js done

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 //Hello World
 /* this is a
 multi-line comment*/
-var x = 1;
-
-if (x === 1) {
-  var x = 2;
-
-  console.log(x);
-  // expected output: 2
-}
-
-console.log(x);
+const myStr = "\t\\SecondLine";
+console.log(myStr);
